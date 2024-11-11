ebf15a04b9a5a8a0179712a795c77b2f0b714bbe
Steven Bethard <steven.bethard@gmail.com>
2024-11-11 12:10:26 -0600

    Skips geo tests when --geojson-dir not provided

--- test/conftest.py
+++ test/conftest.py
@@ -6,10 +6,13 @@ GEOJSON_OPTION = "--geojson-dir"
 
 
 def pytest_addoption(parser):
-    parser.addoption(GEOJSON_OPTION, required=True,
-                     help="Directory containing GeoJson files")
+    parser.addoption(GEOJSON_OPTION, help="Directory containing GeoJson files")
 
 
 @pytest.fixture
 def georeader(request):
-    return normit.geo.GeoJsonDirReader(request.config.getoption(GEOJSON_OPTION))
+    geojson_dir = request.config.getoption(GEOJSON_OPTION)
+    if geojson_dir is not None:
+        return normit.geo.GeoJsonDirReader(geojson_dir)
+    else:
+        pytest.skip(f"skipping as no {GEOJSON_OPTION}=DIR was provided")
